88a9169483d272b91d081f7b25e004eb169ef856
Altertoriel <79248736+system32unknown@users.noreply.github.com>
2024-08-15 14:31:44 +0900

    fix 2

--- src/openfl/net/DatagramSocket.hx
+++ src/openfl/net/DatagramSocket.hx
@@ -320,18 +320,13 @@ class DatagramSocket extends EventDispatcher
 		}
 	}
 
-	override public function addEventListener<T>(type:EventType<T>, listener:T->Void, priority:Int = 0):Void
+	override public function addEventListener(type:String, listener:Dynamic->Void, useCapture:Bool = false, priority:Int = 0,
+			useWeakReference:Bool = false):Void
 	{
-		var dataEvent:EventType<DatagramSocketDataEvent> = DatagramSocketDataEvent.DATA;
-
-		if (type == dataEvent && !this.hasEventListener(dataEvent))
+		super.addEventListener(type, listener, useCapture, priority, useWeakReference);
+		if (type == DatagramSocketDataEvent.DATA)
 		{
-			super.addEventListener(type, listener);
-			CrossByte.current.addEventListener(Event.TICK, __onTickUpdate);
-		}
-		else
-		{
-			super.addEventListener(type, listener);
+			Lib.current.addEventListener(Event.ENTER_FRAME, __onFrameUpdate);
 		}
 	}
 
